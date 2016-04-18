c18804eb45c848f9deb1912676209043bd32bb31
wen <wen@10.0.0.4>
2016-04-17 19:18:51 -0700

    working version 132 ms

--- src/main/java/leetcode/LongestSubstringWithoutRepeatChar.java
+++ src/main/java/leetcode/LongestSubstringWithoutRepeatChar.java
@@ -13,27 +13,31 @@ public class LongestSubstringWithoutRepeatChar {
 	}
 
 	public static int lengthOfLongestSubstring(String s) {
+		
+		if(s.length() == 0) {
+			return 0;
+		}
 		int strlen = s.length();
-		int[][] lenMat = new int[strlen][strlen];
+		//int[][] lenMat = new int[strlen][strlen];
 
 		int maxLen = 1;
 		for (int i = 0; i < strlen; i++) {
 			// i : substring starting from i
-			lenMat[i][i] = 1;
+			int currentMaxLen = 1;
 			Set<String> charSet = new HashSet<String>();
 			charSet.add(String.valueOf(s.charAt(i)));
 			for (int j = i + 1; j < strlen; j++) {
 				// find lenMat[i][j] from lenMat[i][j-1]
 				String currChar = String.valueOf(s.charAt(j));
 				if (charSet.contains(currChar)) {
-					lenMat[i][j] = lenMat[i][j - 1];
+					//lenMat[i][j] = lenMat[i][j - 1];
 					// no more longer substring starting from i
 					break;
 				} else {
-					lenMat[i][j] = lenMat[i][j - 1] + 1;
+					currentMaxLen +=1;
 					charSet.add(currChar);
-					if (lenMat[i][j] > maxLen) {
-						maxLen = lenMat[i][j];
+					if (currentMaxLen > maxLen) {
+						maxLen = currentMaxLen;
 					}
 				}
 			}
